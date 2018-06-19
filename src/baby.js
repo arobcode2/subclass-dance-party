64a066aa65c5855696d270adb9ba80ca5aadc77c
arobcode2 <andrew-c-rob@live.com>
2018-06-18 20:03:26 -0500

    Implement subclasses

---
+++ src/baby.js
@@ -0,0 +1,28 @@
+var baby = function(top, left, timeBetweenSteps) {
+  //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+  makeDancer.call(this, top, left, timeBetweenSteps);
+
+  
+  this.step()
+
+  //var oldStep = blinkyDancer.step;
+  
+};
+
+  baby.prototype = Object.create(makeDancer.prototype);
+
+  baby.prototype.oldStep = makeDancer.prototype.step;
+  
+  baby.prototype.constructor = baby;
+
+  baby.prototype.step = function() {
+    // call the old version of step at the beginning of any call to this new version of step
+     this.oldStep();
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+     this.$node.toggle();
+   };
